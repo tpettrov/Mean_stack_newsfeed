5cb186adcc895ab7810f5a591a718a4533ab94d4
tpettrov <tpettrov@gmail.com>
2017-10-25 23:06:54 +0300

    'test'

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
+var cors = require('cors');
 var ObjectID = mongodb.ObjectID;
 
 var NEWS_COLLECTION = "news";
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
